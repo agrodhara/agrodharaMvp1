6346ff43d0b1317b7476e92edb85ce09a640069d
BhavyaKohli-code <bhavyakohli1209@gmail.com>
2025-05-03 23:52:24 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ db.initializeDatabase()
     });
 
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`✅ Server running on port ${PORT}`);
       console.log(`🚀 API ready at http://localhost:${PORT}`);
     });
