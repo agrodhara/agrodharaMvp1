d9729c1f1fcac260cc079f25baf534b4a72f547e
BhavyaKohli-code <bhavyakohli1209@gmail.com>
2025-05-04 01:57:26 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'fpo_management2',
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 40,
   queueLimit: 0
 });
 
